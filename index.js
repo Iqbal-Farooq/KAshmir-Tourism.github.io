265c656fd57432f1e879efa2d1fe739ebfec8897
iqbal-Farooq <akashfarooq222@gmail.com>
2023-05-14 17:24:08 +0530

    changes done

--- index.js
+++ index.js
@@ -7,19 +7,13 @@ document.querySelector('.toggle').addEventListener('click',()=>{
 
           let element=  document.querySelector('.date');
           let date=new Date();
-          let resdate=date.getFullYear();
-          element.innerHTML=resdate;
-const hours = date.getHours();
-const minutes = date.getMinutes();
-const seconds = date.getSeconds();
-// document.querySelector('#arabic-time').innerHTML=`${hours} : ${minutes} `
-         
+        
 
 
 
          
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', calendar: 'islamic' };
-const arabicDate = new Intl.DateTimeFormat('US', options).format(date);
+const options = { weekday: 'long', year: 'numeric',  day: 'numeric',month: 'long', calendar: 'English' };
+const arabicDate = new Intl.DateTimeFormat('UR', options).format(date);
 console.log(arabicDate);
 document.querySelector('#arabic-date').innerHTML=arabicDate;
 
